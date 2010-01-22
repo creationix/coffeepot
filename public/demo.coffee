9a59b1288041e3e5e8bbc94f2d8ba07b462b1737
Tim Caswell <tim@creationix.com>
2010-01-22 12:02:43 -0600

    Fix the demo code to run in Firefox too.

--- public/demo.coffee
+++ public/demo.coffee
@@ -1,3 +1,31 @@
+source: null
+output: null
+
+setOutput: js =>
+  # Remove the contents
+  while output.firstChild
+    output.removeChild(output.firstChild)
+
+  # put in new contents
+  output.appendChild(document.createTextNode(js))
+
+# Called
+compile: =>
+  code: source.value
+  try
+    js: CoffeePot.compile(code)
+    setOutput(js)
+  catch e
+    setOutput(e.stack)
+    throw e
+
+
+# Recompile after 500ms of idle time after any activity.
+timer: null
+onChange: e =>
+  clearTimeout(timer) if timer
+  timer: setTimeout(compile, 1000)
+
 # Wait for the dom to be built before moving on.
 this.onload: =>
 
@@ -9,31 +37,10 @@ this.onload: =>
   sample: document.getElementById("sample").innerHTML
   sample = sample.substr(1, sample.length)
 
-  # Called
-  compile: =>
-    code: source.value
-    try
-      js: CoffeePot.compile(code)
-      output.innerText = js
-    catch e
-      output.innerText = e.stack
-
   # Fill in the box with the input and call compile
   source.value = sample
   compile()
 
-  # Recompile after 500ms of idle time after any activity.
-  timer: null
-  onChange: e =>
-    console.log("Change")
-    clearTimeout(timer) if timer
-    timer: setTimeout(compile, 1000)
-
   # Bind onkeyup and onchange in the text field
-  source.addEventListener('keyup',onChange)
-  source.addEventListener('change',onChange)
-
-
-
-
-
+  source.addEventListener('keyup', onChange, false)
+  source.addEventListener('change', onChange, false)
--- public/demo.js
+++ public/demo.js
@@ -1,39 +1,49 @@
 (function(){
-
+  var compile, onChange, output, setOutput, source, timer;
+  source = null;
+  output = null;
+  setOutput = function setOutput(js) {
+    // Remove the contents
+    while (output.firstChild) {
+      output.removeChild(output.firstChild);
+    }
+    // put in new contents
+    return output.appendChild(document.createTextNode(js));
+  };
+  // Called
+  compile = function compile() {
+    var code, js;
+    code = source.value;
+    try {
+      js = CoffeePot.compile(code);
+      return setOutput(js);
+    } catch (e) {
+      setOutput(e.stack);
+      throw e;
+    }
+  };
+  // Recompile after 500ms of idle time after any activity.
+  timer = null;
+  onChange = function onChange(e) {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    return timer = setTimeout(compile, 1000);
+  };
   // Wait for the dom to be built before moving on.
   this.onload = function onload() {
-    var compile, onChange, output, sample, source, timer;
+    var sample;
     // Store references to our textareas.
     source = document.getElementById("source");
     output = document.getElementById("output");
     // Load the sample code out of the script tag in the head.
     sample = document.getElementById("sample").innerHTML;
     sample = sample.substr(1, sample.length);
-    // Called
-    compile = function compile() {
-      var code, js;
-      code = source.value;
-      try {
-        js = CoffeePot.compile(code);
-        return (output.innerText = js);
-      } catch (e) {
-        return (output.innerText = e.stack);
-      }
-    };
     // Fill in the box with the input and call compile
     source.value = sample;
     compile();
-    // Recompile after 500ms of idle time after any activity.
-    timer = null;
-    onChange = function onChange(e) {
-      console.log("Change");
-      if (timer) {
-        clearTimeout(timer);
-      }
-      return timer = setTimeout(compile, 1000);
-    };
     // Bind onkeyup and onchange in the text field
-    source.addEventListener('keyup', onChange);
-    return source.addEventListener('change', onChange);
+    source.addEventListener('keyup', onChange, false);
+    return source.addEventListener('change', onChange, false);
   };
 })();
